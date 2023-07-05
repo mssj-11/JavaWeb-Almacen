5675da269b18c509ca1fa3fae7e0517ed9b60c36
Mike <80673026+mssj-11@users.noreply.github.com>
2023-07-05 14:10:11 -0600

    Conexion
    Conexion a la Base de Datos

--- src/java/config/Conexion.java
+++ src/java/config/Conexion.java
@@ -1,9 +1,25 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
- * @author HP
+ * @author HP-MSS
  */
 public class Conexion {
+    
+    public Connection getConexion(){
+        
+        try {
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaweb-almacen", "root", "");
+            return conexion;
+        } catch (SQLException e) {
+            System.out.println("Error: "+e.toString());
+            return null;
+        }
+        
+    }
     
 }
